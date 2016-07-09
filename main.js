9485e829b39349b1904a0254dc9911462738b3dc
SwittS <switt.srikulacheep@gmail.com>
2016-07-09 15:47:57 -0700

    bind key click to specific class and move across the screen

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
+console.log("sanity sanity sanity!");
+
 
 $(document).ready(function() {
-  console.log("sanity sanity sanity");
-
-
-
-
-  $(document).keyup(function(e) { // key click for possible player
-     if(e.which == 48) { // click zero
-       console.log('zero zero zero');
-     } else if(e.which == 49)
-       console.log("one one one");
-
+  $(document).keyup(function(e) {
+    if(e.which == 49) {
+      $(".player1").css({marginLeft: "+=2%"});
+     }
    });
+  $(document).keyup(function(e) {
+    if(e.which == 48) {
+      $(".player2").css({marginLeft: "+=2%"});
+    }
+  });
+
+
+
+
+
+
 
 });
