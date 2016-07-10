37356d5216b1d4e4588368f9697cf2ffcf72df94
SwittS <switt.srikulacheep@gmail.com>
2016-07-10 12:09:28 -0700

    add ability to end game once a player reaches the end of racetrack

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 console.log("sanity sanity sanity!");
 
-
 $(document).ready(function() {
+
+  // player 1 button mash "1"
   $(document).keyup(function(e) {
     if(e.which == 49) {
-      $(".player1").css({marginLeft: "+=2%"});
-     }
-   });
-  $(document).keyup(function(e) {
-    if(e.which == 48) {
-      $(".player2").css({marginLeft: "+=2%"});
+      $(".player1").animate({marginLeft: "+=60"});
+      console.log("test");
+    }
+
+    if ($('.raceTrack1').width() <= $('.player1').offset().left){
+      console.log("yes it works!");
     }
   });
 
-
-
+  // player 2 button "0" mash
+  $(document).keyup(function(e) {
+    if(e.which == 48) {
+      $(".player2").animate({marginLeft: "+=60"});
+      console.log("test2");
+    }
+  });
 
 
 
