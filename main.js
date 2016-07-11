a816572cb28ac3a095fc2f3f853bf57e7e4d0afc
SwittS <switt.srikulacheep@gmail.com>
2016-07-10 23:46:37 -0700

    fixed formatting in javascript

--- main.js
+++ main.js
@@ -5,44 +5,45 @@ $(document).ready(function() {
   var $raceTrack1 = $(".raceTrack1");
   var $raceTrack2 = $(".raceTrack2");
 
+  // reset the game by moving both players back to original position, clears the text, and hides the button
+  $('.btn-primary').on("click", function() {
+    $player1.css({
+      "left": "0"
+    });
+    $player2.css({
+      "left": "0"
+    });
+    $("h2").text("");
+    $("#reset").addClass("hidden");
+  });
   // player 1 button "1" mash
   $(document).keyup(function move(e) {
     if (e.which == 49) {
-      $player1.css("left",("+=40"));
+      $player1.css("left", ("+=40"));
       // if player 1 reaches the end of the track
       if ($raceTrack1.width() <= $player1.offset().left + $player1.outerWidth()) {
         $("h2").text("You saved Matt Damon. Good job! We get more Jason Bourne movies!");
-        $("#singlebutton").removeClass("hidden");
+        $("#reset").removeClass("hidden");
         stopMvmt();
       }
     }
   });
-
   // player 2 button "0" mash
   $(document).keyup(function(e) {
     if (e.which == 48) {
-      $player2.css("left",("+=40"));
+      $player2.css("left", ("+=40"));
       // if player 2 reaches the end of the track
       if ($raceTrack2.width() <= $player2.offset().left + $player2.outerWidth()) {
         $("h2").text("Bad job. You let Jimmy Kimmel capture Matt Damon! No chance for Good Will Hunting 2 now!");
-        $("#singlebutton").removeClass("hidden");
+        $("#reset").removeClass("hidden");
         stopMvmt();
       }
     }
   });
-
-$('.btn-primary').on("click", function(){
-  $player1.css("left",("0"));
-  $player2.css("left",("0"));
-  $("h2").text("     ");
-  $("#singlebutton").addClass("hidden");
-});
-
   // halts the movement after image meets the end of the window!
   function stopMvmt() {
     $player1.clearQueue();
-    $player1.clearQueue();
+    $player2.clearQueue();
     $(document).off("keyup");
   }
-
 });
